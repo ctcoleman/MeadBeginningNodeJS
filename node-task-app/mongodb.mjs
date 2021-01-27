e2eab49e22e359ee9c558a200ac0951a695c1845
ctcoleman <ctcoleman510@gmail.com>
2021-01-26 21:37:03 -0500

    node-task-app: finished mongodb playground - REST APIs next - keep on keepin on

--- node-task-app/mongodb.mjs
+++ node-task-app/mongodb.mjs
@@ -14,37 +14,11 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
   const db = client.db(databaseName)
 
-  db.collection('users').findOne({ _id: new ObjectID('6010a91c6d02dd54d93128fa') }, (err, usr) => {
-    if(err) {
-      return console.log('unable to fetch that user, sir...')
-    }
+  // DELETE ONE OBJECT FROM THE DATABASE
+  db.collection('tasks').deleteOne({ description: 'write personal KB docs' }).then(result => console.log(result)).catch(err => console.log(err))
 
-    console.log(usr)
-  })
-
-  db.collection('users').find({ age: 27 }).toArray((err, usrs) => {
-    if(err) {
-      return console.log('unable to find those users, sir...')
-    }
-
-    console.log(usrs)
-  })
-
-  db.collection('tasks').findOne({ _id: new ObjectID('6010ab4d6c0a5c55fc6b0bc2') }, (err, task) => {
-    if(err) {
-      return console.log('unable to find that task in your list, sir...')
-    }
-
-    console.log(task)
-  })
-
-  db.collection('tasks').find({ completed: false }).toArray((err, tasks) => {
-    if(err) {
-      return console.log('unable to find that task in your list, sir...')
-    }
-
-    console.log(tasks)
-  })
+  // DELETE MULTIPLE OBJECTS FROM THE DATABASE
+  db.collection('users').deleteMany({ age: 27 }).then(result => console.log(result)).catch(err => console.log(err))
 })
 
 
