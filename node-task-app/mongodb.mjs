929389257372c8187ef06853a5e04da0ee6cc75e
ctcoleman <ctcoleman510@gmail.com>
2021-01-26 20:53:26 -0500

    node-task-app: added query to mongodb for user and tasks

--- node-task-app/mongodb.mjs
+++ node-task-app/mongodb.mjs
@@ -4,18 +4,8 @@ import mongodb from 'mongodb'
 const MongoClient = mongodb.MongoClient
 const ObjectID = mongodb.ObjectID
 
-// mongodb objectid:
-// a 4-byte timestamp value, representing the ObjectId’s creation, measured in seconds since the Unix epoch
-// a 5-byte random value
-// a 3-byte incrementing counter, initialized to a random value
-
-
 const connectionURL = `mongodb://127.0.0.1:27017`
 const databaseName = `task-manager`
-
-const id = new ObjectID()
-console.log(id.id)
-console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
   if(err) {
@@ -24,59 +14,37 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
   const db = client.db(databaseName)
 
-  // INSERT ONE
-  db.collection('users').insertOne({
-    _id: id,
-    name: 'snickerz',
-    age: 32
-  }, (error, result) => {
+  db.collection('users').findOne({ _id: new ObjectID('6010a91c6d02dd54d93128fa') }, (err, usr) => {
     if(err) {
-      return console.log('unable to insert this user, sir...')
+      return console.log('unable to fetch that user, sir...')
     }
 
-    console.log(result.ops)
+    console.log(usr)
   })
 
-  // INSERT MANY
-  // db.collection('users').insertMany([
-  //   {
-  //     name: 'Loralai',
-  //     age: 28
-  //   },
-  //   {
-  //     name: 'Bob',
-  //     age:27
-  //   }
-  // ], (err, result) => {
-  //   if(err) {
-  //     return console.log('unable to insert those users, sir...')
-  //   }
+  db.collection('users').find({ age: 27 }).toArray((err, usrs) => {
+    if(err) {
+      return console.log('unable to find those users, sir...')
+    }
 
-  //   return console.log(result.ops)
-  // })
+    console.log(usrs)
+  })
 
+  db.collection('tasks').findOne({ _id: new ObjectID('6010ab4d6c0a5c55fc6b0bc2') }, (err, task) => {
+    if(err) {
+      return console.log('unable to find that task in your list, sir...')
+    }
 
-  // // CHALLANGE
-  // db.collection('tasks').insertMany([
-  //   {
-  //     description: 'write personal KB docs',
-  //     completed: false
-  //   },
-  //   {
-  //     description: 'create layout for KB docs webpage static pages',
-  //     completed: false
-  //   },
-  //   {
-  //     description: 'pick my nose',
-  //     completed: true
-  //   }
-  // ], (err, result) => {
-  //   if(err) {
-  //     return console.log('i wasn\'t able to add those tasks to the list, sir...')
-  //   }
+    console.log(task)
+  })
 
-  //   return console.log(result.ops)
-  // })
+  db.collection('tasks').find({ completed: false }).toArray((err, tasks) => {
+    if(err) {
+      return console.log('unable to find that task in your list, sir...')
+    }
+
+    console.log(tasks)
+  })
 })
 
 
